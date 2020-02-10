186de2e7861d38761577d18f251b3e7ecc2db1e3
adamas01 <adamas.01k@gmail.com>
2020-02-10 18:12:11 +0900

    깃 테스트용 입니다. ^^

---
+++ 2020_01_21/src/Sample2.java
@@ -0,0 +1,7 @@
+public class Sample2{
+	public static void main(String[] args){
+		System.out.println("이름 : 이찬영");
+		System.out.println("전공 : 컴퓨터공학");
+		System.out.print("나이 : 27");
+	}
+}
---
+++ 2020_01_21/src/Sample3.java
@@ -0,0 +1,5 @@
+public class Sample3{
+	public static void main(String[] args){
+		System.out.println("기생충-조여정 ㅋㅋ");
+	}
+}
